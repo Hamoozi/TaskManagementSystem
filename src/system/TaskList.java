10d4285ed5f1775a27746f710161270b71795513
Hamoozi <moguristhebest@gmail.com>
2023-07-15 17:13:56 -0400

    feat: task list

--- src/system/TaskList.java
+++ src/system/TaskList.java
@@ -1,5 +1,47 @@
 package system;
 
-public class TaskList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TaskList {
+    private String name;
+    private String description;
+    private List<Task> tasks;
+
+    public TaskList(String name, String description) {
+        this.name = name;
+        this.description = description;
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    // Add other methods for task list management operations
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
